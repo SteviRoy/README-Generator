dd531e5d54985374046ce9c1dd0af0b20ba1a031
S.P. ROY <sprithi44@gmail.com>
2023-03-02 19:08:48 -0500

    Question 1 added successfully

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    
+  }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
