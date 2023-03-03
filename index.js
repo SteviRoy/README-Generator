3e4de18b466231e02ec89f17cfda1ee37aeac7b6
S.P. ROY <sprithi44@gmail.com>
2023-03-02 19:55:26 -0500

    all questions added

--- index.js
+++ index.js
@@ -14,7 +14,32 @@ const questions = [
     name: 'description',
     message: 'Provide a description of your project:',
   },
-
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage instructions:',
+  },
+  {
+    type: 'input',
+    name: 'credits',
+    message: 'Provide contribution credits:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+  },
 ];
 
 // TODO: Create a function to write README file
